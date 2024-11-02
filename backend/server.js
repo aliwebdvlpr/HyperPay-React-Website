59bece1d692f0cf476623e4697e51ba56efe7588
ToGo <93922191+KitchenBuy@users.noreply.github.com>
2024-11-02 03:17:12 +0300

    modified cors in backend code

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,12 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+    
 app.post('/api/checkout', async (req, res) => {
     const path = '/v1/checkouts';
     const data = querystring.stringify({
