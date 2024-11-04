e67baba1ebc4c957e3a07cef587f021acfc9d065
ToGo <93922191+KitchenBuy@users.noreply.github.com>
2024-11-05 00:34:56 +0300

    fixed the frontend api endpoint

--- frontend/src/CheckoutButton.js
+++ frontend/src/CheckoutButton.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import PaymentForm from './PaymentForm'; // Adjust the path as needed
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://hyper-pay-react-website.vercel.app' : 'http://localhost:3000';
 
 const CheckoutButton = () => {
     const [checkoutData, setCheckoutData] = useState(null); // State to hold checkout response
 
     const handleCheckout = async () => {
         try {
-            const response = await axios.post('http://localhost:3000/api/checkout'); 
+            const response = await axios.post(`${API_URL}/api/checkout`); 
             console.log('Checkout response:', response.data);
             setCheckoutData(response.data); // Update state with response data
         } catch (error) {
