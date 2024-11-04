fae71bddf018f4b9f8e72984a5ccfddd1143a6e6
ToGo <93922191+KitchenBuy@users.noreply.github.com>
2024-11-04 22:51:54 +0300

    corrected cors allowed cors origin

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const querystring = require('querystring');
 
 const allowedOrigins = [
     'https://hyper-pay-react-website-6p7m1s1sr-software-2020s-projects.vercel.app',
+    'https://hyper-pay-react-website-4kwesbmp4-software-2020s-projects.vercel.app',
     'http://localhost:3001' // Include localhost for testing in development
 ];
 
