b4bf87a0ab609db82fbced748afcbbdc566d167c
ToGo <93922191+KitchenBuy@users.noreply.github.com>
2024-11-05 00:23:34 +0300

    deleted the homepage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CheckoutPage from './CheckoutPage';
-import HomePage from './HomePage.js';
 import PaymentForm from './PaymentForm.js';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/" element={<CheckoutPage />} />
                 <Route path="/payment" element={<PaymentForm />} />
             </Routes>
         </Router>
